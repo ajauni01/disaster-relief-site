494e229eb477718b43b6fb316d9fc23410fff69a
ImaniLamla <117410808+ImaniLamla@users.noreply.github.com>
2026-02-18 17:37:33 -0600

    Established a connection from the backend to the database (#2)

--- server/index.js
+++ server/index.js
@@ -14,6 +14,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://leratolamla_db_user:tPBVe3Nr2KmRlwQP@cluster0.knjejmg.mongodb.net/?appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 // Middleware
 app.use("/api/help-requests", helpRoutes);
 app.use("/api/volunteers", volunteerRoutes);
